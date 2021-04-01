f45ded9000e621366f9c192a48dfd3ab395146b0
Joanne Chen <joanne_r_chen@yahoo.com>
2021-04-01 20:32:11 +0100

    project setup complete, fetched items data using useQuery

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="react-scripts" />
+
+declare module "react/jsx-runtime" {
+    export default any;
+}
+
+declare module "react" {
+    export default any;
+}
+
+
+// declare module "react/useState" {
+//     export default any;
+// }
